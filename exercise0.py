d33c412bca52c26bb5c1b6c2bb0a3282c731380e
cudjoeab <cudjoeab@gmail.com>
2019-07-11 15:44:37 -0400

    exercise 0

---
+++ exercise0.py
@@ -0,0 +1,35 @@
+#LISTS
+fav_colours = ['black', 'turqouise', 'yellow']
+family_ages = [25, 27, 34, 40, 41, 46]
+coin_flip = ['tails', 'heads', 'tails', 'tails','tails']
+fav_artists = ['Janelle Monae', 'Tush', 'Lizzo']
+
+#DICTIONARIES 
+
+dictionary = {
+    'burgeon': 'to sprout, bloom, or flourish',
+    'lionize': 'to treat with great importance',
+    'flair':   'a natural talent or unique style'
+}
+
+fav_movies = {
+    'The Lion King': 1994,
+    'Akira': 1988, 
+    'The Shining': 1980,
+    'Us': 2019,
+    'Paprika': 2006
+}
+
+cities = {
+    'Toronto': 2930000,
+    'Orleans': 116668,
+    'Kingston': 666041
+}
+
+siblings = {
+    'Andrew': 46,
+    'Shawn': 41,
+    'Jason': 40, 
+    'Dustin': 34,
+    'Ekua': 25
+}
