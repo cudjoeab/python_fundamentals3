ca2fad0ef615a7ec97993a2ea6056377ca1c9cf2
cudjoeab <cudjoeab@gmail.com>
2019-07-11 23:48:55 -0400

    BitMaker modulo

--- collections-iteration3.py
+++ collections-iteration3.py
@@ -6,3 +6,14 @@ numbers_list= []
 for number in range(1,101):
     numbers_list.append(number)
 print(numbers_list)
+
+#BitMaker's FizzBuzz 
+for number in range(1,101):
+    if number % 3 == 0:
+        print('Bit')
+    elif number % 5 == 0:
+        print('Maker')
+    elif number % 3 == 0 and number % 5 == 0:
+        print ('BitMaker')
+    else: 
+        print(number)
