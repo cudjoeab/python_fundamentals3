6d74920651d880136917d5a51dacacb7b2298e7d
cudjoeab <cudjoeab@gmail.com>
2019-07-12 15:06:40 -0400

    another attempt at exercise 12

--- collections-iteration3.py
+++ collections-iteration3.py
@@ -19,16 +19,37 @@ for number in range(1,101):
         print(number)
 
 #PizzaMaker
+# ask user how many pizzas they want to order 
+def get_pizzas():
+    print('How many pizzas do you want to order?')
+    return int(input())
+print(get_pizzas())
 
-print('How many pizzas do you want to order?')
-quantity =  int(input())
+def get_toppings(pizza):
+    print('How many toppings do you want on pizza{}.'.format(pizza))
+    return (int(input())) 
 
-pizzas = []
-for pizza in pizzas:
-    pizzas.append('pizza' + 1)
-    
-print(pizzas)
 
+
+
+# # have their quantity set the list length 
+# toppings = []
+# for x in quantity: 
+#     print ('How many toppings for pizza{}?'.format(x))
+#     toppings.append(number)
+
+
+# # for every pizza in the list, ask how many toppings 
+
+
+# #confirm that they ordered a pizza with their amount of toppings 
+
+
+# # pizzas = []
+# # for pizza in pizzas:
+# #     pizzas.append(pizza + 1)
+
+# # print(pizzas)
 
 
 
